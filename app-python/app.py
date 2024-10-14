293276808b66841165ae335ad7b1f4869a1fc7e9
Kushan Thamel <kushan.thamel@gmail.com>
2024-10-14 21:22:45 +0100

    modified the python app

--- app-python/app.py
+++ app-python/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify, request
+import psutil
 
 app = Flask(__name__)
 
 @app.route('/index', methods=['GET'])
 def function_flask():
     if(request.method == 'GET'):
-        data = {"data": "Hello World"}
-        return jsonify(data)
+        name = int(psutil.virtual_memory().used / (1024 ** 2))
+        content = (f"Used Memory is {name} MB")
+        return jsonify(content)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9001)
